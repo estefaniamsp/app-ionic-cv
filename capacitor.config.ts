dd5ecd23206b7c537023170b289023714015ef3e
estefaniamsp <estefania.sanchez01@epn.edu.ec>
2024-10-14 20:17:46 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appcv',
+  webDir: 'www'
+};
+
+export default config;
